2f7686a76f4eca38d9385b68e745731929b7ebe6
CoBaum <629494@fom-net.de>
2022-12-02 13:24:33 +0100

    Initial Commit

---
+++ Day1/Day1.py
@@ -0,0 +1,40 @@
+#Teil1:
+def arrayifyCalories(inputFile):
+    file = open(inputFile, 'r')
+    Lines = file.readlines()
+
+    sumCalories = 0
+    array = []
+
+    for line in Lines:
+        if line.strip() == "":
+            array.append(sumCalories)
+            sumCalories = 0
+
+        else:
+            line_value = int(line.strip())
+            sumCalories += line_value
+
+    return array
+
+#Teil2:
+def sumOfHighestThree(array):
+    array.sort(reverse=True)
+    i = 0
+    sumOfCalories = 0
+
+    for i in range(3):
+        sumOfCalories += array[i]
+
+    return sumOfCalories
+
+#Teil1:
+datalist = arrayifyCalories('input day1')
+print(datalist)
+maximum = max(datalist)
+index = datalist.index(maximum)
+print("\n" + "Der " + (str(index+1)) + "te Elf ist die richtige Wahl. Er trägt " + str(maximum) + " Kalorien mit sich.")
+
+#Teil2:
+summe = sumOfHighestThree(datalist)
+print("\n" + "Die drei \"reichsten\" Elfen stemmen insgesamt " + str(summe) + " Kalorien.")
---
+++ Day2/Day2.py
@@ -0,0 +1,63 @@
+#Teil1
+scores_shape = {"X": 1, "Y": 2, "Z": 3}
+scores_outcome = {"loss": 0, "win": 6, "draw": 3}
+#Teil2
+outcome_decider = {"X": "loss", "Y": "draw", "Z": "win"}
+
+#Teil1
+def calculateScore1(inputFile):
+    file = open(inputFile, 'r')
+    totalScore = 0
+    Lines = file.readlines()
+
+    for line in Lines:
+        line_value = line.strip()
+        myChoice = line_value[2]
+        opChoice = line_value[0]
+
+        if ((myChoice == "X" and opChoice == "A") or (myChoice == "Y" and opChoice == "B") or (myChoice == "Z" and opChoice == "C")):
+            outcome = "draw"
+
+        elif ((myChoice == "X" and opChoice == "B") or (myChoice == "Y" and opChoice == "C") or (myChoice == "Z" and opChoice == "A")):
+            outcome = "loss"
+
+        else:
+            outcome = "win"
+
+        totalScore += scores_shape[myChoice] + scores_outcome[outcome]
+
+    return totalScore
+
+#Teil2
+def calculateScore2(inputFile):
+    file = open(inputFile, 'r')
+    totalScore = 0
+    Lines = file.readlines()
+
+    for line in Lines:
+        line_value = line.strip()
+        opChoice = line_value[0]
+        outcomeKey = line_value[2]
+
+        if ((outcomeKey == "X" and opChoice == "B") or (outcomeKey == "Y" and opChoice == "A") or (outcomeKey == "Z" and opChoice == "C")):
+            myChoice = "X"
+
+        elif ((outcomeKey == "X" and opChoice == "C") or (outcomeKey == "Y" and opChoice == "B") or (outcomeKey == "Z" and opChoice == "A")):
+            myChoice = "Y"
+
+        else:
+            myChoice = "Z"
+
+        thisOutcome = outcome_decider[outcomeKey]
+        totalScore += scores_shape[myChoice] + scores_outcome[thisOutcome]
+
+
+    return totalScore
+
+#Teil1
+score1 = calculateScore1('input day2')
+print("\n" + "Nach dem alten Guide komme ich auf eine Punktzahl von insgesamt: " + str(score1) + ".")
+
+#Teil2
+score2 = calculateScore2('input day2')
+print("\n" + "Nach dem neuen Guide komme ich auf eine Punktzahl von insgesamt: " + str(score2) + ".")

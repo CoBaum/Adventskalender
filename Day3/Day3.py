2a59182aad54fbd20d08290151a858415e5ee375
CoBaum <629494@fom-net.de>
2022-12-07 08:31:36 +0100

    Day3

---
+++ Day3/Day3.py
@@ -0,0 +1,65 @@
+#Teil1
+
+scores_priorities = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26}
+
+#Teil1
+def calculatePriorityScore(inputFile):
+    file = open(inputFile, 'r')
+    Lines = file.readlines()
+    totalPriorityScore = 0
+
+    for line in Lines:
+        lineValue = line.strip()
+
+
+        halfLength = len(lineValue)//2
+        firstCompartment = lineValue[:halfLength]
+        secondCompartment = lineValue[halfLength:]
+
+        for i in range(len(firstCompartment)):
+            currentCharacter = firstCompartment[i]
+            if secondCompartment.find(currentCharacter) != -1:
+                priority = currentCharacter
+                if priority.isupper():
+                    totalPriorityScore += scores_priorities[priority.lower()] + 26
+                    break
+                else:
+                    totalPriorityScore += scores_priorities[priority]
+                    break
+
+    return totalPriorityScore
+
+#Teil2
+def calculatePriorityScoreElfGroup(inputFile):
+    file = open(inputFile, 'r')
+    Lines = file.readlines()
+    totalPriorityScoreElfGroup = 0
+
+
+    for i in range(len(Lines)//3):
+        lineArray = []
+        for j in range(3):
+            currentLine = Lines[0].strip()
+            lineArray.append(currentLine)
+            Lines.pop(0)
+        lineArray.sort(key=len)
+        for k in range(len(lineArray[0])):
+            currentCharacter = lineArray[0][k]
+            if (lineArray[1].find(currentCharacter) != -1) & (lineArray[2].find(currentCharacter) != -1):
+                priority = currentCharacter
+                if priority.isupper():
+                    totalPriorityScoreElfGroup += scores_priorities[priority.lower()] + 26
+                    break
+                else:
+                    totalPriorityScoreElfGroup += scores_priorities[priority]
+                    break
+    return totalPriorityScoreElfGroup
+
+
+#Teil1
+score1 = calculatePriorityScore('input day3')
+print(score1)
+
+#Teil2
+score2 = calculatePriorityScoreElfGroup('input day3')
+print(score2)
